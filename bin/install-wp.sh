18406c51d79234575d9b1e7a577c03e956c515f8
Guybrushtr1pw00d1 <165894720+Guybrushtr1pw00d1@users.noreply.github.com>
2024-04-03 11:36:58 +0200

    Update install-wp.sh
    added plugin and address options

--- bin/install-wp.sh
+++ bin/install-wp.sh
@@ -4,20 +4,23 @@
 wp core install \
   --title="Damn Vulnerable WordPress" \
   --admin_user="admin" \
-  --admin_password="admin" \
+  --admin_password="p4ssw0rd" \
   --admin_email="admin@example.com" \
-  --url="http://127.0.0.1:31337/" \
+  --url="http://<ip-address>:31337/" \
   --skip-email
 
 # Update permalink structure.
 # wp option update permalink_structure "/%year%/%monthnum%/%postname%/" --skip-themes --skip-plugins
 
 # Activate plugin.
-wp plugin activate iwp-client 
+#wp plugin activate iwp-client 
 wp plugin activate social-warfare 
-wp plugin activate wp-advanced-search 
-wp plugin activate wp-file-upload 
+#wp plugin activate wp-advanced-search 
+#wp plugin activate wp-file-upload
+wp plugin activate wps-hide-login
 # wp plugin activate wp-time-capsule # Causes error
+wp option update home 'http://<ip-address>:31337/'
+wp option update siteurl 'http://<ip-address>:31337/'
 
 # Update DB
 wp db import dump.sql
